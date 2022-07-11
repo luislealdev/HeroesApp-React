e9626836008892b96b192fd13a4b6847bfa1c8d5
luislealdev <luisrrleal@gmail.com>
2022-07-10 20:07:24 -0500

    Create files with heroes, the function getHeroe.... and the <HeroesList/> component

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,5 +1,11 @@
+import { HeroesList } from "../components";
+
 export const DcPage = () => {
   return (
-    <h1>DcPage</h1>
-  )
-}
+    <>
+      <h1>DcPage</h1>
+      <hr />
+      <HeroesList publisher="DC Comics"/>
+    </>
+  );
+};
