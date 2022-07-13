ce19799e233611766f5bb4f5f69e46e14b042bbb
luislealdev <luisrrleal@gmail.com>
2022-07-12 22:11:13 -0500

    Add private and public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { HeroesRouter } from "../heroes/";
+import { PrivateRouter,PublicRouter } from "./";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRouter />} />
+        <Route
+          path="login"
+          element={
+            <PublicRouter>
+              <LoginPage />
+            </PublicRouter>
+          }
+        />
+        <Route
+          path="/*"
+          element={
+            <PrivateRouter>
+              <HeroesRouter />
+            </PrivateRouter>
+          }
+        />
       </Routes>
     </>
   );
