2185fdafed3bdb73e20e4d461ca4a333f3b96c24
luislealdev <luisrrleal@gmail.com>
2022-07-12 21:11:20 -0500

    Keep the user active

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -5,20 +5,35 @@ import { authReducer } from "./authReducer"
 
 export const AuthProvider = ({children}) => {
 
-    const initialState = {
-        logged: false
+    // const initialState = {
+    //     logged: false
+    // }
+
+    const init = () => {
+      const user = JSON.parse(localStorage.getItem('user'));
+
+      return {
+        logged: !!user,
+        user
+      }
+
     }
 
-    const [authState, dispatch] =useReducer(authReducer, initialState)
+    const [authState, dispatch] = useReducer(authReducer, {}, init)
 
     const login = (name) => {
+
+      const user = {
+        id: 1,
+        name
+      }
+
       const action = {
         type: types.login,
-        payload: {
-          id: 1,
-          name: name
-        }
+        payload: user
       }
+
+      localStorage.setItem('user', JSON.stringify(user));
 
       dispatch(action);
     }
